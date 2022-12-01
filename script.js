c0605d98a238aeb492ce367e20bf282d9a6810f0
Darksied95 <aliyuefe9501@gmail.com>
2022-12-01 18:15:46 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const heading1 = document.querySelector('#question_heading_1')
+const heading2 = document.querySelector('#question_heading_2')
+const heading3 = document.querySelector('#question_heading_3')
+const heading4 = document.querySelector('#question_heading_4')
+const heading5 = document.querySelector('#question_heading_5')
+
+const article1 = document.querySelector('#article_1')
+const article2 = document.querySelector('#article_2')
+const article3 = document.querySelector('#article_3')
+const article4 = document.querySelector('#article_4')
+const article5 = document.querySelector('#article_5')
+
+
